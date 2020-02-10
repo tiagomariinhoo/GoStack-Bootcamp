f4df8b8344bc11e317afa69f61b9337d1cc83f3b
Tiago Marinho <tiago.marinho.797@gmail.com>
2020-02-09 23:30:10 -0300

    acabando primeira parte do modulo02

--- modulo02_gobarber/src/routes.js
+++ modulo02_gobarber/src/routes.js
@@ -1,14 +1,14 @@
-//Não importa o express inteiro, apenas o Router
+// Não importa o express inteiro, apenas o Router
 // const { Router } = require('express');
-import {Router} from 'express'
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({
-    message: "Hello World"
+    message: 'Hello World',
   });
-})
+});
 
 // module.exports = routes;
 export default routes;
