474b2fd43990b6ee15646247a651facd42a57c87
Tiago Marinho <tiago.marinho.797@gmail.com>
2020-03-05 16:05:21 -0300

    Modulo 3 parte 4

--- modulo03_gobarber/src/app/controllers/AppointmentController.js
+++ modulo03_gobarber/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -120,6 +120,35 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    // Checa se o ID é diferente do Usuário que está logado
+    // Já que só quem pode cancelar é o próprio dono do agendamento
+    if (appointment.user_id != req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    // Remove duas horas do horário do agendamento
+    const dateWithSub = subHours(appointment.date, 2);
+
+    // Verifica se a data do agendamento é duas horas a menos que agora
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments two hours in advance.',
+      });
+    }
+
+    // Data atual do cancelamento
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
