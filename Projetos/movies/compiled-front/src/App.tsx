33e69ac0f9452998c3eff0180f1c8dc0c10d9075
tiagomariinhoo <tiago.marinho.797@gmail.com>
2021-02-04 23:54:09 -0300

    compiled-project

---
+++ Projetos/movies/compiled-front/src/App.tsx
@@ -0,0 +1,15 @@
+import { globalAgent } from 'http';
+import React from 'react';
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <div>
+      <h1>Login</h1>
+      <p>Todo: MaterialUI - StackOverflow Emblem</p>
+    </div>
+  </>
+)
+
+export default App;
